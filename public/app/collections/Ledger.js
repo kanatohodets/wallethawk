45460e1abc46d11aaa6e1fb517cbe3475b5b36ab
Ben Tyler <benjamin.tyler@gmail.com>
2015-06-13 23:24:42 +0200

    Fix sorting for ledger items.
    Sort by dateCreated DESC, if they tie then break the tie with dateModified ASC.

--- public/app/collections/Ledger.js
+++ public/app/collections/Ledger.js
@@ -16,7 +16,26 @@ define(function (require, exports, module) {
       });
     },
 
-    comparator: 'date',
+    comparator: function (lineItemA, lineItemB) {
+      var aEpoch = lineItemA.get('dateCreated'),
+          bEpoch = lineItemB.get('dateCreated');
+
+      // dateCreated should really be stored as a proper date, not as
+      // a unix epoch. however, it isn't, so hack into comparable formats.
+      var aDate = moment.unix(aEpoch).format('D-M-YYYY');
+      var bDate = moment.unix(bEpoch).format('D-M-YYYY');
+
+      // break ties with dateModified
+      if (aDate == bDate) {
+        var aModified = lineItemA.get('dateModified'),
+            bModified = lineItemB.get('dateModified');
+
+        // reverse order by the numeric comparison of the epoch values
+        return (+bModified) - (+aModified);
+      }
+
+      return (+bEpoch) - (+aEpoch);
+    },
 
     model: LineItem,
     url: '/api/ledger',
